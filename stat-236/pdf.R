fe3cf956825f7df7971376f2550f780165db506b
Eric M Shen <eric.mm.shen@gmail.com>
2023-12-05 22:50:02 -0500

    edit basic pdf plot

--- stat-236/pdf.R
+++ stat-236/pdf.R
@@ -1,14 +1,24 @@
 library(hypergeo)
 
+# this is really numerically instable
 f_pdf <- function(n, alpha, beta, k) {
   frac <- alpha/(alpha+beta)
   hg <- hypergeo(k-n+1, k+alpha, k+alpha+beta, frac)
+  # normalize by dividing by gamma(k+alpha+beta)
   num <- gamma(alpha+beta) * choose(n-1, k) * (frac ** k) * gamma(k+alpha) * (1/gamma(k+alpha+beta)) * hg
   den <- gamma(alpha)
   return(num/den)
 }
 
-ks <- 0:100
-ps <- f_pdf(100, 1, 5, ks)
-plot(ks, ps, type="h", xlab = "Values", ylab = "Relative Probability Density", yaxt="n",
-     main = "Discrete Probability Distribution")
+n <- 100
+alpha <- 1
+beta <- 5
+
+ks <- 0:n
+ps <- f_pdf(n, alpha, beta, ks)
+plot(ks, ps, type="h", xlab = "Degree", ylab = "Probability Density", yaxt="n", 
+     main = "Probability Distribution, Beta(1, 5)", 
+     cex.main = 1.6, cex.axis = 1.6, cex.lab = 1.6)
+plot(log(ks), log(ps), type="p", xlab = "Log Degree", ylab = "Log Probability Density", yaxt="n", 
+     main = "Log-Log Probability Distribution, Beta(1, 5)", 
+     cex.main = 1.6, cex.axis = 1.6, cex.lab = 1.6)
