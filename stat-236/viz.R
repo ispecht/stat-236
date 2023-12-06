a2c29bdc2c7fe13f2455760ae4267cb84ada31c1
Eric M Shen <eric.mm.shen@gmail.com>
2023-12-06 01:52:28 -0500

    histogram code

--- stat-236/viz.R
+++ stat-236/viz.R
@@ -22,20 +22,32 @@ sim <- function(a, b, n){
 }
 
 # Beta parameters
-a <- 3
-b <- 1
+a <- 1
+b <- 5
 
 # Size of matrix
-n <- 100
+n <- 1000
 
 # Draw an example n x n matrix
-m <- sim(a,b,n)
-g <- graph_from_adjacency_matrix(m, mode = "undirected")
-plot(g, vertex.color = "white", vertex.label = NA, vertex.label.cex = 0.8)
+hist = rep(0, n)
+for (i in 1:100) {
+  if (i %% 10 == 0) {
+    print(i)
+  }
+  m <- sim(a,b,n)
+  g <- graph_from_adjacency_matrix(m, mode = "undirected")
+  degrees <- degree_distribution(g)
+  degrees <- c(degrees, rep(0, n-length(degrees)))
+  hist <- hist + degrees
+}
+
+# m <- sim(a,b,n)
+# g <- graph_from_adjacency_matrix(m, mode = "undirected")
+# plot(g, vertex.color = "white", vertex.label = NA, vertex.label.cex = 0.8)
 
 # Plot a histogram of degrees
-degrees <- degree_distribution(g)
-degrees <- c(degrees, rep(0, n-length(degrees)))
-plot(1:n, degrees, type="h", xlab = "Degree", ylab = "Count", yaxt="n", 
-     main = "Histogram of Node Degrees, Beta(3, 3)", 
-     cex.main = 1.6, cex.axis = 1.6, cex.lab = 1.6)
+# degrees <- degree_distribution(g)
+# degrees <- c(degrees, rep(0, n-length(degrees)))
+plot(1:n, degrees, type="h", xlab = "Degree", ylab = "Count", yaxt="n",
+     main = "Histogram of Node Degrees, Beta(3, 3)",
+     cex.main = 1, cex.axis = 1, cex.lab = 1)
